defb6cd433242668e8428ae6fe6555a53af7253e
Roberto-Parra <w.parrav@uniandes.edu.co>
2022-04-26 08:17:50 -0400

    Nuevo dockerfile

--- app/main.py
+++ app/main.py
@@ -1,15 +1,26 @@
+from asyncio import Handle
 from fastapi import FastAPI
+import json
+import os
 
 app = FastAPI()
 
-@app.get('/')
+'''@app.get('/')
 def mensaje():
     return {"Hello":"Mundo"}
 
 @app.get('/login')
 def mensaje():
-    return "Ingrese sus datos"
+    return "Ingrese sus datos"'''
 
-@app.get('/users/{user_id}')
+'''@app.get('/users/{user_id}')
 def mensaje(user_id):
-    return f"Ingrese sus datos, el id del usuario es: {user_id}"
+    return f"Ingrese sus datos, el id del usuario es: {user_id}"'''
+
+def handle(data=None) -> dict:
+    event = json.loads(os.environ['REQ'])
+    print(event)
+    return {"result":"Mensaje recibido"}
+
+if __name__ == "__main__":
+    handle()
